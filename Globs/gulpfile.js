5d5850bfe7cfac483dd3044c9b30571497acdb8f
cheesyburrito30 <pvernon30@gmail.com>
2020-07-04 10:14:45 -0400

    added comments to globs gulpfile

--- Globs/gulpfile.js
+++ Globs/gulpfile.js
@@ -5,11 +5,11 @@ const { src, dest, series } = require('gulp'),
 
 function compileJavascript() {
     return src(['./assets/**/*.js', '!./assets/vendor/dontUseMe.js'])
-        .pipe(concat('scripts.js'))
-        .pipe(dest(`./dist/js`))
-        .pipe(uglify())
-        .pipe(rename('scripts.min.js'))
-        .pipe(dest('./dist/js'))
+        .pipe(concat('scripts.js')) //combine sream into scripts.js
+        .pipe(dest(`./dist/js`)) //send unminified combined file to dist/js
+        .pipe(uglify()) //minify stream
+        .pipe(rename('scripts.min.js')) //rename stream to scripts.min.js
+        .pipe(dest('./dist/js')) //send minified stream to dist/js
 }
 
 exports.default = series(compileJavascript)
